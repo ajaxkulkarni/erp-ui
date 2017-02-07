13b0b33fa29ca347fde086c1580b829cb3338e95
ajaxkulkarni <ajinkyashiva@gmail.com>
2017-02-07 11:20:23 +0530

    latest changes

--- js/ang/salary.js
+++ js/ang/salary.js
@@ -119,15 +119,13 @@ angular.module("app").controller('employeeSalary', function ($scope, userService
     }
     
     $scope.getTotal = function() {
-        if($scope.employee == null) {
-            return;
-        }
-        var total = ($scope.user.company.basic.percentage/100)*($scope.employee.salary);
+        
+        var total = ($scope.user.company.basic.percentage/100)*($scope.salary);
         var i = 0;
         for(i=0;i<$scope.user.company.salaryInfo.length;i++) {
-            total = total + ($scope.user.company.salaryInfo[i].percentage/100)*($scope.user.company.basic.percentage/100)*($scope.employee.salary);
+            total = total + ($scope.user.company.salaryInfo[i].percentage/100)*($scope.user.company.basic.percentage/100)*($scope.salary);
         }
-        total = $scope.employee.salary - total;
+        total = $scope.salary - total;
         console.log("Total:" + total);
         return total;
     }
@@ -135,19 +133,23 @@ angular.module("app").controller('employeeSalary', function ($scope, userService
 
     $scope.save = function () {
 
-        if($scope.user.company.basic == null || $scope.user.company.basic.percentage < 0) {
-            $scope.basicError = true;
-            $("#basic").focus();
+        if($scope.employee == null) {
+            $scope.salaryError = true;
             return;
         }
-         
+        
+        if($scope.salary == null || $scope.salary < 0) {
+            $scope.salaryError = true;
+            return;
+        }
         $scope.showProgress = true;
-        $scope.dataObj.user = $scope.user
-        userService.callService($scope, "/addSalaryStructure").then(function (response) {
+        $scope.employee.financial.salary = $scope.salary;
+        $scope.dataObj.user = $scope.employee;
+        userService.callService($scope, "/addSalary").then(function (response) {
             //$.skylo('end');
             $scope.showProgress = false;
             $scope.response = response;
-            userService.showResponse($scope, "Salary structure updated successfully!");
+            userService.showResponse($scope, "Employee Salary updated successfully!");
             //$("#myModal").modal('show');
             //console.log(response.user.company);
 
